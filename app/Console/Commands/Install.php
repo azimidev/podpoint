5b3fdd31d9ff3ff491935ac16ab02702545915f8
Hassan Azimi <hassanazimi@users.noreply.github.com>
2020-04-08 01:18:30 +0100

    Done install command

--- app/Console/Commands/Install.php
+++ app/Console/Commands/Install.php
@@ -3,7 +3,6 @@
 namespace App\Console\Commands;
 
 use Illuminate\Console\Command;
-use Illuminate\Support\Facades\Artisan;
 
 class Install extends Command
 {
@@ -37,16 +36,11 @@ class Install extends Command
      */
     public function handle()
     {
-        // copy(base_path('.env.example'), base_path('.env'));
+        copy(base_path('.env.example'), base_path('.env'));
         $this->info('Copied .env.example to .env <info>✔</info>');
 
-        // Artisan::call('key:generate');
+        $this->call('key:generate');
         $this->info('Ran php artisan key:generate <info>✔</info>');
-
-        $this->call('migrate');
-        $this->info('Ran php artisan migrate <info>✔</info>');
-        // front yarn
-        // front build
 
         $this->line($this->output());
     }
